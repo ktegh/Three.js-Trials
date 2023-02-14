37bc9dd7c2dfbd78c181bbcc38833a056d1cac7c
ktegh <113183732+ktegh@users.noreply.github.com>
2023-02-13 18:07:41 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+//import { OrbitControls } from 'three';
+
+function main(){
+  const canvas = document.querySelector('#c');
+  const render = new THREE.WebGLRender({canvas});
+}
